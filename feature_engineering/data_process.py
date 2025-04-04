0f0eb54c72ab847ace87cd5cfb9bec96d6b2db03
LDLZM <1924730014@qq.com>
2025-04-04 15:54:21 +0800

    123

--- feature_engineering/data_process.py
+++ feature_engineering/data_process.py
@@ -7,14 +7,10 @@ import torch
 import dgl
 import random
 import os
-import time
-import argparse
-import pickle
 import matplotlib.pyplot as plt
 import networkx as nx
 import scipy.sparse as sp
 from sklearn.preprocessing import LabelEncoder
-
 from tqdm import tqdm
 from sklearn.preprocessing import StandardScaler
 # from . import *
@@ -54,24 +50,6 @@ def featmap_gen(tmp_df=None):
     return pd.DataFrame(post_fe)
 
 
-def sparse_to_adjlist(sp_matrix, filename):
-    """
-    Transfer sparse matrix to adjacency list
-    :param sp_matrix: the sparse matrix
-    :param filename: the filename of adjlist
-    """
-    # add self loop
-    homo_adj = sp_matrix + sp.eye(sp_matrix.shape[0])
-    # create adj_list
-    adj_lists = defaultdict(set)
-    edges = homo_adj.nonzero()
-    for index, node in enumerate(edges[0]):
-        adj_lists[node].add(edges[1][index])
-        adj_lists[edges[1][index]].add(node)
-    with open(filename, 'wb') as file:
-        pickle.dump(adj_lists, file)
-    file.close()
-
 
 def set_seed(seed):
     random.seed(seed)
@@ -81,93 +59,6 @@ def set_seed(seed):
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
-def k_neighs(
-    graph: dgl.DGLGraph,
-    center_idx: int,
-    k: int,
-    where: str,
-    choose_risk: bool = False,
-    risk_label: int = 1
-) -> torch.Tensor:
-    """return indices of risk k-hop neighbors
-
-    Args:
-        graph (dgl.DGLGraph): dgl graph dataset
-        center_idx (int): center node idx
-        k (int): k-hop neighs
-        where (str): {"predecessor", "successor"}
-        risk_label (int, optional): value of fruad label. Defaults to 1.
-    """
-    target_idxs: torch.Tensor
-    if k == 1:
-        if where == "in":
-            neigh_idxs = graph.predecessors(center_idx)
-        elif where == "out":
-            neigh_idxs = graph.successors(center_idx)
-
-    elif k == 2:
-        if where == "in":
-            subg_in = dgl.khop_in_subgraph(
-                graph, center_idx, 2, store_ids=True)[0]
-            neigh_idxs = subg_in.ndata[dgl.NID][subg_in.ndata[dgl.NID] != center_idx]
-            # delete center node itself
-            neigh1s = graph.predecessors(center_idx)
-            neigh_idxs = neigh_idxs[~torch.isin(neigh_idxs, neigh1s)]
-        elif where == "out":
-            subg_out = dgl.khop_out_subgraph(
-                graph, center_idx, 2, store_ids=True)[0]
-            neigh_idxs = subg_out.ndata[dgl.NID][subg_out.ndata[dgl.NID] != center_idx]
-            neigh1s = graph.successors(center_idx)
-            neigh_idxs = neigh_idxs[~torch.isin(neigh_idxs, neigh1s)]
-
-    neigh_labels = graph.ndata['label'][neigh_idxs]
-    if choose_risk:
-        target_idxs = neigh_idxs[neigh_labels == risk_label]
-    else:
-        target_idxs = neigh_idxs
-
-    return target_idxs
-
-
-def count_risk_neighs(
-    graph: dgl.DGLGraph,
-    risk_label: int = 1
-) -> torch.Tensor:
-
-    ret = []
-    for center_idx in graph.nodes():
-        neigh_idxs = graph.successors(center_idx)
-        neigh_labels = graph.ndata['label'][neigh_idxs]
-        risk_neigh_num = (neigh_labels == risk_label).sum()
-        ret.append(risk_neigh_num)
-
-    return torch.Tensor(ret)
-
-
-def feat_map():
-    tensor_list = []
-    feat_names = []
-    for idx in tqdm(range(graph.num_nodes())):
-        neighs_1_of_center = k_neighs(graph, idx, 1, "in")
-        neighs_2_of_center = k_neighs(graph, idx, 2, "in")
-
-        tensor = torch.FloatTensor([
-            edge_feat[neighs_1_of_center, 0].sum().item(),
-            # edge_feat[neighs_1_of_center, 0].std().item(),
-            edge_feat[neighs_2_of_center, 0].sum().item(),
-            # edge_feat[neighs_2_of_center, 0].std().item(),
-            edge_feat[neighs_1_of_center, 1].sum().item(),
-            # edge_feat[neighs_1_of_center, 1].std().item(),
-            edge_feat[neighs_2_of_center, 1].sum().item(),
-            # edge_feat[neighs_2_of_center, 1].std().item(),
-        ])
-        tensor_list.append(tensor)
-
-    feat_names = ["1hop_degree", "2hop_degree",
-                  "1hop_riskstat", "2hop_riskstat"]
-
-    tensor_list = torch.stack(tensor_list)
-    return tensor_list, feat_names
 
 
 if __name__ == "__main__":
@@ -182,7 +73,6 @@ if __name__ == "__main__":
     data.replace(np.nan, 0, inplace=True)
     data.to_csv(os.path.join(DATADIR, 'S-FFSDneofull.csv'), index=None)
     data = pd.read_csv(os.path.join(DATADIR, 'S-FFSDneofull.csv'))
-
     data = data.reset_index(drop=True)
     out = []
     alls = []
@@ -216,36 +106,3 @@ if __name__ == "__main__":
         feat_data.to_numpy()).to(torch.float32)
     dgl.data.utils.save_graphs(DATADIR + "graph-S-FFSD.bin", [g])
 
-    # generate neighbor riskstat features
-    for file_name in ['S-FFSD']:
-        print(
-            f"Generating neighbor risk-aware features for {file_name} dataset...")
-        graph = dgl.load_graphs(DATADIR + "graph-" + file_name + ".bin")[0][0]
-        graph: dgl.DGLGraph
-        print(f"graph info: {graph}")
-
-        edge_feat: torch.Tensor
-        degree_feat = graph.in_degrees().unsqueeze_(1).float()
-        risk_feat = count_risk_neighs(graph).unsqueeze_(1).float()
-
-        origin_feat_name = []
-        edge_feat = torch.cat([degree_feat, risk_feat], dim=1)
-        origin_feat_name = ['degree', 'riskstat']
-
-        features_neigh, feat_names = feat_map()
-        # print(f"feature neigh: {features_neigh.shape}")
-
-        features_neigh = torch.cat(
-            (edge_feat, features_neigh), dim=1
-        ).numpy()
-        feat_names = origin_feat_name + feat_names
-        features_neigh[np.isnan(features_neigh)] = 0.
-
-        output_path = DATADIR + file_name + "_neigh_feat.csv"
-        features_neigh = pd.DataFrame(features_neigh, columns=feat_names)
-        scaler = StandardScaler()
-        # features_neigh = np.log(features_neigh + 1)
-        features_neigh = pd.DataFrame(scaler.fit_transform(
-            features_neigh), columns=features_neigh.columns)
-
-        features_neigh.to_csv(output_path, index=False)
